1d3ba6f55a45c358c96f815104edc146be167f56
Serpent7776 <Serpent7776@gmail.com>
2025-05-03 15:12:07 +0200

    Add test for image that isn't multiple of 4

--- test.asm
+++ test.asm
@@ -4,16 +4,19 @@ DEFAULT REL
 section .data
 	testno: dq 0
 align 64
+	;                      R     G     B     A
 	purple4x4: times 16 db 0xff, 0x00, 0xff, 0xff
 	white4x4:  times 16 db 0xff, 0xff, 0xff, 0xff
+	purple5x5: times 25 db 0xff, 0x00, 0xff, 0xff
+	white5x5:  times 25 db 0xff, 0xff, 0xff, 0xff
 
 section .text
 global _start
 extern vxdiff
 
-%macro TEST 5
-	lea rdi, [purple4x4]
-	lea rsi, [white4x4]
+%macro TEST 7
+	lea rdi, [%6]
+	lea rsi, [%7]
 	mov rdx, %1
 	mov rcx, %3
 	mov r8, %2
@@ -27,14 +30,15 @@ extern vxdiff
 %endmacro
 
 _start:
-	TEST 4, 4, 4, 4, 16
-	TEST 1, 1, 1, 1, 1
-	TEST 1, 4, 1, 4, 4
-	TEST 2, 2, 4, 4, 4
-	TEST 1, 1, 1, 1, 1
-	TEST 4, 4, 1, 1, 16
-	TEST 1, 1, 4, 4, 1
-	TEST 3, 4, 4, 3, 12
+	TEST 4, 4, 4, 4, 16, purple4x4, white4x4
+	TEST 1, 1, 1, 1, 1, purple4x4, white4x4
+	TEST 1, 4, 1, 4, 4, purple4x4, white4x4
+	TEST 2, 2, 4, 4, 4, purple4x4, white4x4
+	TEST 1, 1, 1, 1, 1, purple4x4, white4x4
+	TEST 4, 4, 1, 1, 16, purple4x4, white4x4
+	TEST 1, 1, 4, 4, 1, purple4x4, white4x4
+	TEST 3, 4, 4, 3, 12, purple4x4, white4x4
+	TEST 5, 5, 5, 5, 25, purple5x5, white5x5
 .exit_ok:
 	xor rbx, rbx
 .exit:
