2d86329c4fa0068280a1f2ee5ecb6bf0dbef7905
Serpent7776 <Serpent7776@gmail.com>
2025-09-22 18:44:51 +0200

    Use commit as x label

--- chart.R
+++ chart.R
@@ -5,7 +5,9 @@ library(dplyr)
 
 data <- read_csv("y.csv", col_names = c("command", "mean", "stddev", "median", "user", "system", "min", "max"))
 
-p <- ggplot(data, aes(x = 1:nrow(data))) +
+data <- data %>% mutate(command = factor(command, levels = command))
+
+p <- ggplot(data, aes(x = command)) +
   geom_line(aes(y = mean, color = "Mean", group = 1), linewidth = 0.8) +
   geom_errorbar(aes(ymin = mean - stddev, ymax = mean + stddev, color = "Mean ± StdDev"),
                 width = 0.2, alpha = 0.7) +
@@ -19,6 +21,7 @@ p <- ggplot(data, aes(x = 1:nrow(data))) +
   scale_color_manual(values = c("Mean" = "blue", "Mean ± StdDev" = "blue",
                                "Min" = "green", "Max" = "red")) +
   theme_minimal() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, hjust = 1))
 
 ggsave("benchmark_plot.png", p, width = 10, height = 6, dpi = 300)
--- chart.sh
+++ chart.sh
@@ -4,4 +4,4 @@ set -e
 # nasm -g -f elf64 -o v.o v.asm
 nasm -g -f elf64 -o d.o vxdiff.asm
 ld -o vx v.o d.o
-hyperfine -N -i --export-csv x.csv ./vx
+hyperfine -N -i --export-csv x.csv --command-name "$(git rev-parse HEAD)" ./vx
